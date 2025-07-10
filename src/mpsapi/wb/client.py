6f14b5d0cab286ffe27d5222eb76b33053d9b7d4
Dmitry Drozdov <ds.drozdov@gmail.com>
2025-07-10 13:41:09 +0400

    ref

--- src/mpsapi/wb/client.py
+++ src/mpsapi/wb/client.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import date
 
 import aiohttp
@@ -10,22 +11,24 @@ class WBClient:
         self.api_key = api_key
         self.session = session or aiohttp.ClientSession(headers={"Authorization": self.api_key})
 
+    async def close(self):
+        await self.session.close()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.close()
+
     async def get_seller_info(self):
         url = f"{self.BASE_URL}/api/v1/seller-info"
         params = {}
         async with self.session.get(url, params=params) as resp:
-            resp.raise_for_status()
-            data = await resp.json()
-            return data
+            return await resp.json()
 
     async def get_news(self, dt: date = None):
         url = f"{self.BASE_URL}/api/communications/v2/news"
         dt = dt.isoformat() if dt else date.today().isoformat()
         params = {"from": dt}
         async with self.session.get(url, params=params) as resp:
-            resp.raise_for_status()
-            data = await resp.json()
-            return data
-
-    async def close(self):
-        await self.session.close()
+            return await resp.json()
